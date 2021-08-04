02047cf8c3b108de7d1382fc80e93c61fc3b05a9
Hammad-23 <hsyed2633@gmail.com>
2021-08-04 20:44:39 +0500

    learning php

---
+++ first.php
@@ -0,0 +1,91 @@
+<?php
+
+  // Variables in PHP
+
+  // $naam = "Syed Hammad Ahmed";
+  // echo($naam);
+  // $age = "23";
+  
+  // echo("My age is ".$age)
+
+  // Constant variables in PHP
+  // 1) first letter of variable name must be alphabet or underscore 
+  // 2) Can't use $ sign with constant variable name
+  // 3) Constant variables are global variables
+
+  // define("num",500);
+  // // echo num;
+  // $num2 = 100;
+  // echo (num+$num2);
+
+
+  // if else statements
+
+  // if(num<1000){
+  //   echo("Hello");
+  // }else{
+  //   echo("world");
+  // };
+
+  // Ternary operator
+
+  // $naam="Hammad";
+
+  //  echo(  $naam=="Hammad" ? "Sahi hai " : "Galat hai") ;
+
+
+  // For Loop
+
+  // for($a=1; $a<=10; $a++){
+  //   if($a==5){
+  //     continue;
+  //   }
+  //   echo($a. " <br>");
+  // }
+
+  // Goto statements
+
+  //  for($a=1; $a<=10; $a++){
+  //   if($a==5){
+  //     goto abc;
+  //   }
+  //   echo($a. " <br>");
+  // }
+ 
+  // abc:
+  // echo("Hello World")
+
+  // Functions in PHP
+
+//   function fullName(){
+//     echo("My Name is Syed Hammad");
+//     echo("<br>");
+//     echo("My age is 20");
+//   }
+// fullName();
+
+// function sumNum($a,$b){
+
+//    $total=$a+$b;
+
+//  return $total;
+
+// }
+// echo(  sumNum(200,200));
+
+
+// Passing Function Arguments by refrence
+
+// $a ="Programming";
+// function hello(&$a){
+//     $a = "hello world";
+//     echo("Inside Function--> ".$a);
+// }
+
+// hello($a);
+// echo("<br>");
+// echo("outside function--> ".$a)
+ 
+
+
+?>
